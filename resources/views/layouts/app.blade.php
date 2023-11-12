0744a9d4f57f2c9753c84f89cf8ca3d7e1c0ad79
Beyondscript <nafiul1351@gmail.com>
2023-11-12 18:57:20 +0600

    initial release

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
   @vite('resources/css/app.css')
-  gg
   
   <!-- Nucleo Icons -->
   @vite('public/assets/css/nucleo-icons.css')
