81c65ae8452e64dd9ace77630b351b3fc933470f
Beyondscript <nafiul1351@gmail.com>
2024-09-19 13:00:29 +0600

    7th commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#">
 
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="robots" content="noindex, nofollow">
+  <meta property="og:image" content="{{ asset('assets/img/facebookimage.png') }}">
 
   <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
   <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
